dd55aef024d2a85634842af459caab1c5ca03eae
AnastasiiaGanieva <69317903+AnastasiiaGanieva@users.noreply.github.com>
2021-03-20 20:15:16 +0400

    Update BodyIndexService.java

--- BodyIndexService.java
+++ BodyIndexService.java
@@ -1,9 +1,9 @@
 public class BodyIndexService {
-    public double calculate(int massa,int rost) {
-            double index = (double) massa/(rost*rost);
-int a= (int) (index*1000);
-double c = (double) a/1000;
-            return c;}
-        }
-
+    public double calculate(int massa, int rost) {
+        double index = (double) massa / (rost * rost);
+        int a = (int) (index * 1000);
+        double c = (double) a / 1000;
+        return c;
+    }
+}
 
